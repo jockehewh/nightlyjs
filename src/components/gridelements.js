a6902dc42c87fdfc31797e8298e75401acb8871f
jockehewh <xavier.belenus@gmail.com>
2020-02-13 20:28:14 +0100

    adjusting

--- src/components/gridelements.js
+++ src/components/gridelements.js
@@ -8,7 +8,6 @@ export const gridElement = (context, element)=>{
     let imgurl = element.body.split(':img:')
     title(outerDiv, element.title, 6)
     let img = addImage(outerDiv, imgurl[1], 'new component exemple')
-    console.dir(img)
       outerDiv.style.gridColumnStart = 1,
       outerDiv.style.gridColumnEnd = 3
       outerDiv.style.textAlign = "unset"
@@ -16,18 +15,10 @@ export const gridElement = (context, element)=>{
   }else if(/(:img:)$/.test(element.body)){
       let details = element.body.split(':img:')
       title(outerDiv, element.title, 3)
-      console.log(details)
       paragraph(outerDiv, details[0])
       let imgurl = details[1]
       let img = addImage(outerDiv, imgurl)
       context.parentElement.style.minHeight = 'min-content'
-      /* if(img.width > context.clientWidth*2){
-      outerDiv.style.gridColumnStart = 1,
-      outerDiv.style.gridColumnEnd = 3
-      outerDiv.style.textAlign = "unset"
-      }else{
-        img.width = context.clientWidth+'px'
-      } */
     }else{
       title(outerDiv, element.title, 3)
       paragraph(outerDiv, element.body)
