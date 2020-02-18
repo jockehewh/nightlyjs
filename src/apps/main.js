49957dd2aef215ef13e183ae00f7e613216a55b3
jockehewh <xavier.belenus@gmail.com>
2020-02-18 17:18:52 +0100

    adding some style

--- src/apps/main.js
+++ src/apps/main.js
@@ -3,16 +3,28 @@ import '../components/css/main.css'
 import {SPAMinHeight} from '../components/SPAMinHeight'
 import { showLoader } from '../components/loader'
 import { SPAFullHeight } from '../components/SPAFullHeight'
-function main(){
+async function main(){
   const loadingApp = showLoader()
   var appDesign = Math.floor(Math.random()*2+1)
   fetch('./body.json').then(res=>{
    return res.json()
- }).then(t =>{
+ }).then(async t =>{
    loadingApp.remove()
-   if(appDesign === 1)return document.app = SPAFullHeight(document.body, t)
-   if(appDesign === 2)return document.app = SPAMinHeight(document.body, t)
- }).catch(err=>{
+   if(appDesign === 1)return document.app = await SPAFullHeight(document.body, t)
+   if(appDesign === 2)return document.app = await SPAMinHeight(document.body, t)
+ }).then((app)=>{
+  let sectionOneText = app.querySelector('.section-0 p').innerText.split('').map(c=>{
+    let span = document.createElement('span')
+    span.innerText = c
+    return span
+  })
+  app.querySelector('.section-0 p').innerText = ''
+  sectionOneText.forEach(element=>{
+    app.querySelector('.section-0 p').appendChild(element)
+  })
+
+
+}).catch(err=>{
    console.log(err)
  })
 }
